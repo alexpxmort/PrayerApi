0ab0795e583c7caaaf7ec7f2c24addc472b436db
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2023-10-21 11:00:10 -0300

    Update index.js

--- src/fastify/routes/homilia-diaria/index.js
+++ src/fastify/routes/homilia-diaria/index.js
@@ -1,15 +1,11 @@
-import cache from "../../../helpers/cache.js";
+
 import { formatDate,getCurrentDate } from "../../../helpers/date.js";
 import { getVideoHomiliaDiariaUseCase } from "../../../usecases/index.js";
 
 const handler = async (fastify,opts) =>{
   fastify.get('/',async (req,reply) => {
     let videoCode = '';
-    if(cache.get(`homilia_diaria${formatDate(getCurrentDate(),'dd-mm-yyyy','-')}`)){
-        videoCode = cache.get(`homilia_diaria${formatDate(getCurrentDate(),'dd-mm-yyyy','-')}`)
-        return reply.send({idVideo:videoCode})
-    }
-
+    
     videoCode = await getVideoHomiliaDiariaUseCase.execute()
     return  reply.send({idVideo:videoCode})
   })
